0b659045315f349e629222ef0dbee15e1d6b1f16
rahuljayakrishnan <rahuljayd@gmail.com>
2022-04-01 23:08:27 +0530

    connect to ganache

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,4 +1,7 @@
+import json
 import solcx
+import web3
+
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
 
@@ -18,4 +21,13 @@ compiled_sol = solcx.compile_standard(
     },
     solc_version="0.8.0",
 )
-print(compiled_sol)
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+w3 = web3(web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 5777
+my_address = "0xBDefedA4A8662680661FAf3E313ED962C1C971f3"
+private_key = "0xa5cd7357171b17d2f1e40c3698b4b1c96ff4df2b71fbe6e609edaae8a016957d"
